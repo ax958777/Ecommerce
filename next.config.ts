fbc7df231e8e8d8845062bc503004435918466de
Ax958777 <ax958777@gmail.com>
2025-01-04 15:30:59 -0700

    ignore -type-build

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   eslint:{
     ignoreDuringBuilds:true,
   },
-  ignoreBuildErrors:true,
+  typescript:{
+    ignoreBuildErrors:true,
+  }
+  
 };
 
 export default nextConfig;
