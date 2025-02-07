878ac5a1b66fb985f67902cd3fdd1123fd55e38f
Ax958777 <ax958777@gmail.com>
2025-02-07 10:35:28 -0700

    create-payment-intent

---
+++ src/app/api/create-payment-intent/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-12-18.acacia",
+});
+
+const calculateOrderAmount = (items) => {
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
+  });
+  return total;
+};
+
+export async function POST(req: Request) {
+  const { items } = await req.json();
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+}
